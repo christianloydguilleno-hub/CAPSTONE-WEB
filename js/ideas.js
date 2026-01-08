664243cc15b93f38ea1e717c91684e9026bc2432
Loyd.Dev <christianloydguilleno@gmail.com>
2026-01-08 09:50:35 +0800

    Implement idea rendering and submission logic
    Add functionality to render and submit ideas in the form.

--- js/ideas.js
+++ js/ideas.js
@@ -1,1 +1,29 @@
+const list = document.getElementById("ideaList");
+const form = document.getElementById("ideaForm");
+
+function renderIdeas() {
+  list.innerHTML = "";
+  Storage.get("ideas").forEach(i => {
+    list.innerHTML += `
+      <li class="list-group-item d-flex justify-content-between">
+        ${i.title}
+        <a href="tasks.html?idea=${i.id}" class="btn btn-sm btn-primary">Tasks</a>
+      </li>`;
+  });
+}
+
+form.onsubmit = e => {
+  e.preventDefault();
+  const ideas = Storage.get("ideas");
+  ideas.push({
+    id: Storage.generateId(),
+    title: title.value,
+    desc: desc.value
+  });
+  Storage.set("ideas", ideas);
+  form.reset();
+  renderIdeas();
+};
+
+renderIdeas();
 
