95f7c7c70f522cca61ba95986c54cce194a068d3
Loyd.Dev <christianloydguilleno@gmail.com>
2026-01-08 09:49:31 +0800

    Update teams.js

--- js/teams.js
+++ js/teams.js
@@ -1,1 +1,21 @@
+const list = document.getElementById("teamList");
+const form = document.getElementById("teamForm");
+
+function renderTeams() {
+  list.innerHTML = "";
+  Storage.get("teams").forEach(t => {
+    list.innerHTML += `<li class="list-group-item">${t.name}</li>`;
+  });
+}
+
+form.onsubmit = e => {
+  e.preventDefault();
+  const teams = Storage.get("teams");
+  teams.push({ id: Storage.generateId(), name: teamName.value });
+  Storage.set("teams", teams);
+  form.reset();
+  renderTeams();
+};
+
+renderTeams();
 
